21c849a9389ee3022e98b5c29e386d12ad9712db
Varun Verma <varunverma823@gmail.com>
2024-07-31 23:13:12 +0530

    fetch user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://workwave-umlf.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
@@ -35,7 +35,7 @@ const App = () => {
       }
     };
     fetchUser();
-  }, [isAuthorized]);
+  }, [setIsAuthorized, setUser]);
   return (
     <>
       <BrowserRouter>
