31ca98cffda651029dd2443743f9f36fe0e012a3
Varun Verma <varunverma823@gmail.com>
2024-08-10 22:48:21 +0530

    cors error

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({ path: "./config/config.env" });
 app.use(
     cors({
-      origin: 'http://localhost:5173' ,
+      origin: 'https://work-wave01.netlify.app' ,
       method: ["GET", "POST", "DELETE", "PUT"],
       credentials: true,
       // allowedHeaders:['Content-Type','Authorization']
